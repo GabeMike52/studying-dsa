5078234af6d44f67a7c1cb2529d778f9be418660
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2025-01-10 11:53:21 -0300

    Added an optimized version of the prime number algorithm

--- Math Algorithms/isPrime.js
+++ Math Algorithms/isPrime.js
@@ -1,3 +1,4 @@
+// O(n)
 function isPrime(n) {
     if (n < 2) {
         return false;
@@ -10,7 +11,15 @@ function isPrime(n) {
     return true;
 }
 
-console.log(isPrime(1));
-console.log(isPrime(7));
-console.log(isPrime(22));
-console.log(isPrime(3));
+// O(sqrt(n))
+function optimizedIsPrime(n) {
+    if (n < 2) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
